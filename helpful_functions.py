b96a9cb03b0fda77c26bd17a86951b9d355beeeb
Joan Mas Castella <33759606+Joanmascastella@users.noreply.github.com>
2024-11-01 10:08:43 +0100

    preparing structure of project

--- helpful_functions.py
+++ helpful_functions.py
@@ -3,8 +3,11 @@ import torch
 def get_device():
     if torch.mps.is_available():
         device = torch.device("mps")
+        print("Using MPS")
     elif torch.cuda.is_available():
         device = torch.device("cuda:0")
+        print("Using CUDA")
     else:
         device = torch.device("cpu")
+        print("Using CPU")
     return device
--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 # Importing Libraries
 import torch
 
-
 # Importing Classes
 from helpful_functions import get_device
 
 
+def main():
+
+    # Defining Data Paths
+    train_file = "./data/train.csv"
+    test_file = "./data/test.csv"
+    submission_file = "./data/sample_submission.csv"
+
+    # Creating Instance of Device
+    device = get_device()
+
+    # Processing Data
+    print("Step 1. Processing Data")
+
+
+
+    # Autoencoder
+    print("Step 2. Loading And Running Autoencoder")
+
+
+    # Classifier
+    print("Step 3. Loading Classifier & Training & Validating")
+
+
+
+
+if __name__ == '__main__':
+    main()
